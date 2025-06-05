956c67c7b46b594b0e99c57baed433aabe4519d0
Laura Parra <lauraparratorres10@gmail.com>
2025-06-04 21:46:00 -0500

    Creacion del cuaderno basico del proyecto S7 Creacion de la aplicación app.py, Y actulización del readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # mi-proyecto-s7
 Este repositorio almacena el proyecto final del sprint 7 sobre herramientas de desarrollo de software
+
+# Portafolio proyecto final sprint 7
+Autor: Laura Natalia Parra Torres
+
+# Proyecto S7
+Creación y gestión de un entorno virtual de Python y el desarrollo de una aplicación web.
+Para un conjunto de datos de anuncios de vemtas de coches, en la cual se puede acceder a gráficas
+de dispersión e histogramas para el analisis de los datos.
---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+car_data = pd.read_csv('vehicles_us.csv')
+hist_button = st.button('Construir histograma')
+disp_button = st.button('Construir gráfico de dispersión')
+
+if hist_button:  # al hacer clic en el boton
+    # escribir un mensaje
+    st.write(
+        'Creación de un histograma para el conjunto de anuncions de venta de coches')
+
+    # crear un histograma
+    hist = px.histogram(car_data, x="odometer")
+
+    # mostrar un gráfico Pllotly interactivo
+    st.plotly_chart(hist, use_container_width=True)
+
+if disp_button:
+    st.write(
+        'Creación de un gráfico de dispersión para el conjunto de anunciones de venta de coches')
+
+    disp = px.scatter(disp, x="odometer", y='price')
+
+    st.plotly_chart(disp,  use_container_width=True)
+
+# crear una casilla de verificacion
+build_histogram = st.checkbox('Construir un histograma')
+
+if build_histogram:
+    st.write('Construir un histograma para la columna odómetro')
